2d4634648f0941ed1acb2f579321f77e38346326
Thisisartelias <Eliaspeters1234@gmail.com>
2026-03-01 13:09:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -98,3 +98,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if(hamburger && navLinks){
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
